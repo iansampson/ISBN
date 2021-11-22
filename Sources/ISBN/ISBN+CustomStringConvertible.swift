12a0ff66c17b3ef0a3c27cb73f226e6bbd3d140d
Ian Sampson <ian_sampson@brown.edu>
2021-11-22 13:35:55 -0800

    Conform ISBN to CustomStringConvertible

---
+++ Sources/ISBN/ISBN+CustomStringConvertible.swift
@@ -0,0 +1,12 @@
+//
+//  ISBN+CustomStringConvertible.swift
+//  
+//
+//  Created by Ian Sampson on 2021-11-22.
+//
+
+extension ISBN: CustomStringConvertible {
+    public var description: String {
+        string
+    }
+}
